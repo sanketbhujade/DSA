8a0a6fe565bc5a0002fbcc87c168948000e4c9a8
sank7 <sanketbhujade1@gmail.com>
2022-10-16 18:55:46 +0530

    maths folder added

---
+++ maths/sum_of_first_n_natural_numbers.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+int optimized(int n)
+{
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    return (n*(n+1))/2;   // in single turn
+}   //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+int naturalnum_sum(int n)
+{   int sum=0;
+    for(int i=1;i<=n;i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+int main() {
+    int n;
+    cout<<"enter number"<<endl;
+    cin>>n;
+    cout<<"sum is "<< naturalnum_sum(n)<<"\n output with optimal solution"<<optimized(n);
+
+return 0;
+}
